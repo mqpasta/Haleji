1164f186e39dc17c268dae60c9a910d5738edc5d
mqpasta <mqpasta@gmail.com>
2021-09-20 21:23:03 +0500

    disable db scripts

--- Haleji.Base/database/run-script.sh
+++ Haleji.Base/database/run-script.sh
@@ -2,8 +2,8 @@
 
 #sleep 20s
 
- /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Admin.1234 -d master -i DBCreation.sql
- sleep 1s
- /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Admin.1234 -d Haleji -i InsertTransType.sql
- sleep 1s
- /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Admin.1234 -d Haleji -i combine_sps.sql
+# /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Admin.1234 -d master -i DBCreation.sql
+# sleep 1s
+# /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Admin.1234 -d Haleji -i InsertTransType.sql
+# sleep 1s
+# /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Admin.1234 -d Haleji -i combine_sps.sql
